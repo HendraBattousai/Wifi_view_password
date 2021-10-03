bf8a4785e5cb42cdde14836017f807cc78cea499
Hendra Battousai <71735045+Rahendra007@users.noreply.github.com>
2021-10-03 10:29:45 +0700

    Add files via upload

---
+++ WifiPassView.bat
@@ -0,0 +1,14 @@
+@echo off
+echo          view wifi password          
+echo @author Hendra Battousai
+setlocal enabledelayedexpansion
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show profile') do ( 
+    set "ssid=%%~a" 
+    call :getpwd "%%ssid:~1%%" 
+    )
+pause
+:getpwd
+set "ssid=%*"
+for /f "tokens=2 delims=:" %%i in ('netsh wlan show profile name^="%ssid:"=%" key^=clear ^|findstr /C:"Key Content"') do (
+         echo Wifi Name : %ssid%         Password :%%i)
+    echo "----------------------------------------------------------------"
